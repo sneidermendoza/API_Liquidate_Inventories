3c8c3938d854212cfdb72c0c730ef826b63c08e1
sneider mendoza <mariasol0304@gmail.com>
2024-03-20 23:10:54 -0500

    se creo los primeros modelos, usuario y rol

---
+++ Liquidate_Inventories/apps/users/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class Role(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None, **extra_fields):
+        if not email:
+            raise ValueError('El campo de correo electrónico es obligatorio.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Los superusuarios deben tener is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Los superusuarios deben tener is_superuser=True.')
+
+        return self.create_user(email, name, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, null=True)  # Agregar el campo last_name
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
