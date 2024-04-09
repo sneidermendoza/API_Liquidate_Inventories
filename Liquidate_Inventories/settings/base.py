b0c36b1cc3b4a973f8d43768363744a7616bf33f
sneider mendoza <mariasol0304@gmail.com>
2024-04-09 14:26:29 -0500

    mejoras

--- Liquidate_Inventories/settings/base.py
+++ Liquidate_Inventories/settings/base.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django.apps import AppConfig
 import os
 
-AppConfig.default = False
+# AppConfig.default = False
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
