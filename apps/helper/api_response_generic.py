b70f31f743fa41e3f41230da77cb1dde590527e3
sneider mendoza <mariasol0304@gmail.com>
2024-04-15 12:37:39 -0500

    update...

--- apps/helper/api_response_generic.py
+++ apps/helper/api_response_generic.py
@@ -6,5 +6,4 @@ def api_response(serializer_data, message=None, status_code=status.HTTP_200_OK):
                          'data': serializer_data,
                          'status' : status_code},
                         status=status_code)
-    
     return response
