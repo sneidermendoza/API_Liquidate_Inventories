0978564c5673fe93d9a4393100daa0bb9860368c
Sneider Mendoza <mariasol0304@gmail.com>
2024-09-10 23:07:21 -0500

    ahora muestra la api de facturacion devuelve el nombre del negosio y el nombre del atributo

--- apps/billing/api/serializers/billing_serializer.py
+++ apps/billing/api/serializers/billing_serializer.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
-
 from apps.billing.models import Billings
+from apps.inventories.models import Inventories
+from apps.generic_tables.models import Attributes
+from apps.business.models import Business  # Asegúrate de importar el modelo correspondiente a Business
 
 class BillinSerializer(serializers.ModelSerializer):
     attribute_name = serializers.CharField(source='attribute.name', read_only=True)
+    business_name = serializers.CharField(source='inventory.business.name_business', read_only=True)
 
     class Meta:
         model = Billings
-        exclude = ('state', 'created_date', 'modified_date', 'deleted_date',)
+        exclude = ('state', 'modified_date', 'deleted_date',)
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        # Puedes agregar más personalización aquí si lo necesitas
+        return representation
